b3a56a59aa98351664ef637f0ffbc22c06ed8cd0
Alban Bajrami <albanba@uio.no>
2025-02-19 18:56:21 +0100

    Second commit

---
+++ letterboxdrandomizer/src/main/java/com/alban/letterboxdrandomizer/LetterboxdrandomizerApplication.java
@@ -0,0 +1,13 @@
+package com.alban.letterboxdrandomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterboxdrandomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterboxdrandomizerApplication.class, args);
+	}
+
+}
---
+++ letterboxdrandomizer/src/test/java/com/alban/letterboxdrandomizer/LetterboxdrandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alban.letterboxdrandomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterboxdrandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
